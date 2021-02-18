fdfeef0b7340c411160799f35497ba829a8e4199
tduyen.86@gmail.com <tduyen.86@gmail.com>
2021-02-18 15:13:28 +0700

    done fan

--- JavaObject/src/com/company/fan/MainFan.java
+++ JavaObject/src/com/company/fan/MainFan.java
@@ -2,16 +2,19 @@ package com.company.fan;
 
 public class MainFan {
     public static void main(String[] args) {
+
         Fan fan1 = new Fan();
         fan1.setSpeed(fan1.FAST);
         fan1.setRadius(10);
         fan1.setColor("yellow");
+        fan1.setOn(true);
         fan1.display();
+
         Fan fan2 = new Fan();
         fan2.setSpeed(fan2.MEDIUM);
         fan2.setRadius(5);
         fan2.setColor("blue");
-        fan2.setTurn(false);
+        fan2.setOn(false);
         fan2.display();
     }
 }
